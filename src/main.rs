298a71b36686681f86e8dabde9994b4a957c9841
Ji Ho Koo <jihokoo@gmail.com>
2015-01-18 19:48:54 -0500

    add code to start server and temporary dependency workaround

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+extern crate iron;
+
+use iron::prelude::*;
+use iron::status;
+
 fn main() {
-    println!("Hello, world!");
+    Iron::new(|&: _: &mut Request| {
+        Ok(Response::with((status::Ok, "Hello world!")))
+    }).listen("localhost:3000").unwrap();
 }
